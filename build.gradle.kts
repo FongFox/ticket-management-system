9c37fb9a3725c1c1e1461c74d4f38912e948c13b
Tran Gia Nguyen Phong <phong.tgn.coder@gmail.com>
2025-07-03 17:13:26 +0700

    chore: init new project,add hello world controller to testing

---
+++ build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.7"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.hibernate.orm") version "6.6.18.Final"
+	id("org.graalvm.buildtools.native") version "0.10.6"
+	id("org.asciidoctor.jvm.convert") version "3.3.2"
+	id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
+}
+
+group = "com.fongfox"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+extra["springAiVersion"] = "1.0.0"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("io.github.wimdeblauwe:htmx-spring-boot-thymeleaf:4.0.1")
+	implementation("org.springframework.ai:spring-ai-markdown-document-reader")
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+//	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
+	}
+}
+
+hibernate {
+	enhancement {
+		enableAssociationManagement = true
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	outputs.dir(project.extra["snippetsDir"]!!)
+}
+
+tasks.asciidoctor {
+	inputs.dir(project.extra["snippetsDir"]!!)
+	dependsOn(tasks.test)
+}
---
+++ src/main/java/com/fongfox/ticketmanagementsystem/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.fongfox.ticketmanagementsystem.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HelloController
+ * <p>
+ * Hello Controller to test controller
+ * <p>
+ * Version 1.0
+ * Date: 7/3/2025
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE         AUTHOR       DESCRIPTION
+ * -------------------------------------
+ * 7/3/2025      FongFox      Create
+ */
+@RestController
+@RequestMapping("api/")
+public class HelloController {
+    @GetMapping("Hello")
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello World From Spring boot! (Exclude security)");
+    }
+}
